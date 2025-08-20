08ffbd32c005d23fa7d6f2343a5b0f6d9799923d
Vadim Meshcheryakov <painassasin@icloud.com>
2025-08-21 00:52:25 +0300

    Send email support

--- mailing/views.py
+++ mailing/views.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.contrib.messages.context_processors import messages
+from django.core.mail import send_mail
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from django.contrib import messages
@@ -12,5 +14,12 @@ class SendMailView(CreateView):
 
     def form_valid(self, form):
         response = super().form_valid(form)
+
+        send_mail(
+            subject=self.object.title,
+            message=self.object.body,
+            recipient_list=[self.object.recipient],
+            from_email=settings.DEFAULT_FROM_EMAIL,
+        )
         messages.success(self.request, 'Сообщение отправлено')
         return response
